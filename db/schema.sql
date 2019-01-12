192bd6110c59df10e0872f3dad32c02e4cbfde64
hoang <hoangcao@mail.weber.edu>
2019-01-12 01:45:46 -0700

    modify database

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
-CREATE DATABASE burger_db;
+CREATE DATABASE IF NOT EXISTS burger_db;
 
 USE burger_db;
+
+DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burger_name NVARCHAR(30) NOT NULL,
-    devour BOOLEAN,
+    devour BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
